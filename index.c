854f81749999a5e27837b66c4730b3632c9c464d
Mohd Zaid <zaid.tab123@gmail.com>
2022-06-10 12:09:24 +0530

    ✨ add struct to code

--- index.c
+++ index.c
@@ -5,10 +5,57 @@
 
 char AddStudent();
 char CheckAttendance();
+char* getUser(long int regno);
+
+struct subject
+{
+  char* name;
+  long int hoursConducted;
+};
+
+struct user
+{
+  char* name;
+  long int rno;
+  long int att[7];
+};
 
 int main(){
   // AddStudent();
   CheckAttendance();
+}
+char* getUser(long int regno){
+  FILE *std; //Student Data
+  std = fopen("./data/student-data.csv","r");
+  if(!std){
+    printf("Can't access the file.");
+  }
+  else{
+    char buffer[1024];
+    int row=0,column=0;
+    while (fgets(buffer,1024,std))
+    {
+      column=0;
+      row++;
+      if (row==0){
+        continue;
+      }
+      char* value = strtok(buffer,",");
+      long int temp = strtoul(value,NULL,10); //int of value
+      if(regno==temp){
+        while (value){
+        if(column == 1){
+          return value;
+        }
+        column++;
+        value = strtok(NULL, ",");
+        continue;
+      }
+      }
+      
+    }
+    fclose(std);
+  }
 }
 
 char CheckAttendance(){
@@ -18,7 +65,7 @@ char CheckAttendance(){
   int rno;
   printf("Enter Registration Number:");
   scanf("%d",&rno);
-
+  
   std = fopen("./data/student-data.csv","r");
   atd = fopen("./data/attendance-data.csv","r");
   
@@ -26,79 +73,117 @@ char CheckAttendance(){
     printf("Can't access the file.");
   }
   else{
+    struct user currentUser;
     char buffer[1024];
-    long int attendance[7],classConducted[7];
+    struct subject sub[7];
+
+    currentUser.name = getUser(rno);
+    currentUser.rno = rno;
+    printf("\n%s\n",currentUser.name);
+
     int row=0,column=0;
     while (fgets(buffer,1024,atd))
     {
       char* value = strtok(buffer,",");
       column=0;
-      if(row!=0){
-        long int temp = strtoul(value,NULL,10);
+      if(row==0){
+        while(value){
+          if(column == 1){
+            sub[0].name=value;
+          }
+          if(column == 2){
+            sub[1].name=value;
+          }
+          if(column == 3){
+            sub[2].name=value;
+          }
+          if(column == 4){
+            sub[3].name=value;
+          }
+          if(column == 5){
+            sub[4].name=value;
+          }
+          if(column == 6){
+            sub[5].name=value;
+          }
+          if(column == 7){
+            sub[6].name=value;
+          }
+          column++;
+          value = strtok(NULL, ",");
+          continue;
+        }
+      }
+      else if(row==1){
+        while(value){
+          if(column == 1){
+            sub[0].hoursConducted=strtoul(value,NULL,10);
+          }
+          if(column == 2){
+            sub[1].hoursConducted=strtoul(value,NULL,10);
+          }
+          if(column == 3){
+            sub[2].hoursConducted=strtoul(value,NULL,10);
+          }
+          if(column == 4){
+            sub[3].hoursConducted=strtoul(value,NULL,10);
+          }
+          if(column == 5){
+            sub[4].hoursConducted=strtoul(value,NULL,10);
+          }
+          if(column == 6){
+            sub[5].hoursConducted=strtoul(value,NULL,10);
+          }
+          if(column == 7){
+            sub[6].hoursConducted=strtoul(value,NULL,10);
+          }
+          column++;
+          value = strtok(NULL, ",");
+          continue;
+        }
+      }
+      
+      
 
+      else{
+        long int temp = strtoul(value,NULL,10);
         if(temp==rno){
           while (value){ //Store value of the no of hours attended in an array
               if(column==1){
-                attendance[0]=strtoul(value,NULL,10);
+                currentUser.att[0]=strtoul(value,NULL,10);
                 }
               if(column==2){
-                attendance[1]=strtoul(value,NULL,10);
+                currentUser.att[1]=strtoul(value,NULL,10);
                 }
               if(column==3){
-                attendance[2]=strtoul(value,NULL,10);
+                currentUser.att[2]=strtoul(value,NULL,10);
                 }
               if(column==4){
-                attendance[3]=strtoul(value,NULL,10);
+                currentUser.att[3]=strtoul(value,NULL,10);
                 }
               if(column==5){
-                attendance[4]=strtoul(value,NULL,10);
+                currentUser.att[4]=strtoul(value,NULL,10);
                 }
               if(column==6){
-                attendance[5]=strtoul(value,NULL,10);
+                currentUser.att[5]=strtoul(value,NULL,10);
                 }
               if(column==7){
-                attendance[6]=strtoul(value,NULL,10);
+                currentUser.att[6]=strtoul(value,NULL,10);
                 }
             
             column++;
             value = strtok(NULL, ",");
           }
         }
-      }
-      else{
-        while(value){
-          if(column == 1){
-              classConducted[0]=strtoul(value,NULL,10);
-              }
-          if(column == 2){
-            classConducted[1]=strtoul(value,NULL,10);
-            }
-          if(column == 3){
-            classConducted[2]=strtoul(value,NULL,10);
-            }
-          if(column == 4){
-            classConducted[3]=strtoul(value,NULL,10);
-            }
-          if(column == 5){
-            classConducted[4]=strtoul(value,NULL,10);
-            }
-          if(column == 6){
-            classConducted[5]=strtoul(value,NULL,10);
-            }
-          if(column == 7){
-            classConducted[6]=strtoul(value,NULL,10);
-            }  
-          column++;
-          value = strtok(NULL, ",");
-          continue;
-        }
-      }
+      }      
       row++;
     }
+    
     for(int i=0;i<7;i++){
-      float att; 
-      att=attendance[i]*100/classConducted[i];
-      printf("Subject %d : %.2f %%\n",i+1,att);
+      float att;
+      att=currentUser.att[i]*100/sub[i].hoursConducted;
+      printf("%s\n",sub[i].name);
+      //printf("%s : %.2f %%\n",sub[i].name,att);
     }
   }
 }
